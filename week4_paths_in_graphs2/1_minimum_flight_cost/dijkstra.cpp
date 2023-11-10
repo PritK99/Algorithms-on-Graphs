36bfd42224bfb29adc2e84a17741005f52890632
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2023-11-10 20:53:34 +0530

    #19/29 test case failed

--- week4_paths_in_graphs2/1_minimum_flight_cost/dijkstra.cpp
+++ week4_paths_in_graphs2/1_minimum_flight_cost/dijkstra.cpp
@@ -1,30 +1,77 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <climits>
 
-using std::vector;
-using std::queue;
-using std::pair;
-using std::priority_queue;
+using namespace std;
 
-long long distance(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, int t) {
-  //write your code her
-  return -1;
+vector <long long> djikstras(vector<vector<int>> &adj, vector<vector<int>> &cost, int s)
+{
+    vector <long long> distance (adj.size());
+    vector <bool> visited (adj.size());
+
+    for (int i = 0; i < adj.size(); i++)
+    {
+        //we use INT_MIN here since our priority queue is Max Heap by default
+        distance[i] = INT_MIN;
+    }
+
+    distance[s] = 0;
+    pair <long long, long long> p;
+    p.first = 0;
+    p.second = s;
+    priority_queue <pair <long long, long long>> pq;
+    pq.push(p);
+    visited[p.second]=1;
+
+    while (!pq.empty())
+    {
+        p = pq.top();
+        pq.pop();
+
+        for (int i = 0; i < adj[p.second].size(); i++)
+        {
+            if (visited[adj[p.second][i]] == 0)
+            {
+                pair <long long, long long> temp;
+                temp.first = -cost[p.second][i];
+                temp.second = adj[p.second][i];
+                pq.push(temp);
+                visited[adj[p.second][i]] = 1;
+            }
+            //edge relaxation
+            distance[adj[p.second][i]] = max(distance[adj[p.second][i]], distance[p.second] - cost[p.second][i]);
+        }
+    }
+    
+    for (int i = 0; i < distance.size(); i++)
+    {
+      if (distance[i] == INT_MIN)
+      {
+        distance[i]=1;
+      }
+      distance[i]*=-1 ;
+    }
+    return distance;
 }
 
-int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  vector<vector<int> > cost(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y, w;
-    std::cin >> x >> y >> w;
-    adj[x - 1].push_back(y - 1);
-    cost[x - 1].push_back(w);
-  }
-  int s, t;
-  std::cin >> s >> t;
-  s--, t--;
-  std::cout << distance(adj, cost, s, t);
+int main()
+{
+    int num_vertices, num_edges;
+    cin >> num_vertices >> num_edges;
+    vector<vector<int>> adj(num_vertices, vector<int>());
+    vector<vector<int>> cost(num_edges, vector<int>());
+    for (int i = 0; i < num_edges; i++)
+    {
+        int x, y, w;
+        cin >> x >> y >> w;
+        adj[x - 1].push_back(y - 1);
+        cost[x - 1].push_back(w);
+    }
+    int s,t;
+    cin >> s >> t;
+    vector <long long> shortest_path = djikstras(adj, cost, s-1);
+
+    cout << shortest_path[t-1];
+    return 0;
 }

e7e2cd24da7251f03fcdb1bf53c386c94611615c
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2023-11-17 09:42:27 +0530

    Adding partial solutions for week 4: #4 failing in 3rd question

--- week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.cpp
+++ week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.cpp
@@ -1,19 +1,58 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using std::vector;
 
-int negative_cycle(vector<vector<int> > &adj, vector<vector<int> > &cost) {
-  //write your code here
+int negative_cycle(vector<vector<int>> &adj, vector<vector<int>> &cost)
+{
+  int v = adj.size();
+  vector<bool> visited(v);
+
+  for (int m = 0; m < v; m++)
+  {
+    vector<int> dist(v, INT_MAX);
+    dist[m] = 0;
+
+    for (int k = 0; k < v - 1; k++)
+    {
+      // Run v-1 interations
+
+      for (int i = 0; i < v; i++)
+      {
+        for (int j = 0; j < adj[i].size(); j++)
+        {
+          if (dist[i] + cost[i][j] < dist[adj[i][j]] && dist[i] != INT_MAX)
+          {
+            dist[adj[i][j]] = dist[i] + cost[i][j];
+          }
+        }
+      }
+    }
+
+    for (int i = 0; i < v; i++)
+    {
+      for (int j = 0; j < adj[i].size(); j++)
+      {
+        if (dist[i] + cost[i][j] < dist[adj[i][j]] && dist[i] != INT_MAX)
+        {
+          return 1;
+        }
+      }
+    }
+  }
+
   return 0;
 }
 
-int main() {
+int main()
+{
   int n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  vector<vector<int> > cost(n, vector<int>());
-  for (int i = 0; i < m; i++) {
+  vector<vector<int>> adj(n, vector<int>());
+  vector<vector<int>> cost(n, vector<int>());
+  for (int i = 0; i < m; i++)
+  {
     int x, y, w;
     std::cin >> x >> y >> w;
     adj[x - 1].push_back(y - 1);
--- week4_paths_in_graphs2/3_exchanging_money/shortest_paths.cpp
+++ week4_paths_in_graphs2/3_exchanging_money/shortest_paths.cpp
@@ -1,41 +1,86 @@
 #include <iostream>
-#include <limits>
 #include <vector>
-#include <queue>
+#include <climits>
 
 using std::vector;
-using std::queue;
-using std::pair;
-using std::priority_queue;
 
-void shortest_paths(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, vector<long long> &distance, vector<int> &reachable, vector<int> &shortest) {
-  //write your code here
+vector<int> negative_cycle(vector<vector<int>> &adj, vector<vector<int>> &cost, int s)
+{
+  int v = adj.size();
+  vector<bool> visited(v);
+
+  vector<int> dist(v, INT_MAX);
+  dist[s] = 0;
+
+  for (int k = 0; k < v - 1; k++)
+  {
+    // Run v-1 interations
+
+    for (int i = 0; i < v; i++)
+    {
+      for (int j = 0; j < adj[i].size(); j++)
+      {
+        if (dist[i] + cost[i][j] < dist[adj[i][j]] && dist[i] != INT_MAX)
+        {
+          dist[adj[i][j]] = dist[i] + cost[i][j];
+        }
+      }
+    }
+  }
+
+  while(1)
+  {
+    vector <int> temp = dist;
+    for (int i = 0; i < v; i++)
+    {
+      for (int j = 0; j < adj[i].size(); j++)
+      {
+        if (dist[i] == INT_MIN || dist[i] + cost[i][j] < dist[adj[i][j]])
+        {
+          dist[adj[i][j]] = INT_MIN;
+        }
+      }
+    }
+    if (temp == dist)
+    {
+      break;
+    }
+  }
+
+  return dist;
 }
 
-int main() {
-  int n, m, s;
+int main()
+{
+  int n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  vector<vector<int> > cost(n, vector<int>());
-  for (int i = 0; i < m; i++) {
+  vector<vector<int>> adj(n, vector<int>());
+  vector<vector<int>> cost(n, vector<int>());
+  for (int i = 0; i < m; i++)
+  {
     int x, y, w;
     std::cin >> x >> y >> w;
     adj[x - 1].push_back(y - 1);
     cost[x - 1].push_back(w);
   }
+  int s;
   std::cin >> s;
-  s--;
-  vector<long long> distance(n, std::numeric_limits<long long>::max());
-  vector<int> reachable(n, 0);
-  vector<int> shortest(n, 1);
-  shortest_paths(adj, cost, s, distance, reachable, shortest);
-  for (int i = 0; i < n; i++) {
-    if (!reachable[i]) {
-      std::cout << "*\n";
-    } else if (!shortest[i]) {
-      std::cout << "-\n";
-    } else {
-      std::cout << distance[i] << "\n";
+  vector<int> dist = negative_cycle(adj, cost, s-1);
+
+  for (int i = 0; i < dist.size(); i++)
+  {
+    if (dist[i] == INT_MAX)
+    {
+      std::cout << "*";
     }
+    else if (dist[i] == INT_MIN)
+    {
+      std::cout << "-";
+    }
+    else
+    {
+      std::cout << dist[i];
+    }
+    std::cout << std::endl;
   }
 }
